b87da491a99cd0794d2df364c7d602651d0898f9
TimTran8 <tim_tran@live.ca>
2018-10-26 20:53:42 -0700

    Add files via upload

---
+++ Lyreka_xcodeProject/Lyreka_xcodeProject/PlaylistViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlaylistViewController.swift
+//  sample_c
+//
+//  Created by Li heng Ou on 10/26/18.
+//  Copyright © 2018 Li heng Ou. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToMainViewController(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lyreka_xcodeProject/Lyreka_xcodeProject/PopupViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PopupViewController.swift
+//  sample_c
+//
+//  Created by Li heng Ou on 10/26/18.
+//  Copyright © 2018 Li heng Ou. All rights reserved.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //Set background color to light grey
+        self.view.backgroundColor = UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 0.8)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closePopup(_ sender: UIButton) {
+        self.view.removeFromSuperview()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
