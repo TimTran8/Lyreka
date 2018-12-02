d265bcd7c4b9c0eb19c250ecb238855ebe27c179
Dante Barr <dbarr@asb9700-e03.csil.sfu.ca>
2018-12-02 13:36:33 -0800

    UI fix difficult button

--- Lyreka_xcodeProject/Lyreka_xcodeProject/PopupViewController.swift
+++ Lyreka_xcodeProject/Lyreka_xcodeProject/PopupViewController.swift
@@ -36,7 +36,11 @@ class PopupViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+    }
+    
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscapeLeft
     }
